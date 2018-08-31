ea545d1f6bb3170c4e3265500e9e0757e9993c0b
Gavin McFarland <gavin@mindthetic.com>
2018-08-31 11:37:39 +0100

    Added another test to support idents

--- .tape.js
+++ .tape.js
@@ -1,16 +1,28 @@
 module.exports = {
-  "postcss-sequence": {
-    basic: {
-      message: "supports basic usage",
-      options: {
-        units: [
-          {
-            abbr: "<a>fs",
-            pattern: "a * 1",
-            output: "calc(var(--font-size-<a>))"
-          }
-        ]
-      }
-    }
-  }
+	"postcss-sequence": {
+		basic: {
+			message: "supports basic usage",
+			options: {
+				units: [
+					{
+						abbr: "gu",
+						pattern: "n * 4",
+						output: "px"
+					}
+				]
+			}
+		},
+		idents: {
+			message: "Supports identifiers",
+			options: {
+				units: [
+					{
+						abbr: "<a>fs",
+						pattern: "a * 1",
+						output: "calc(var(--font-size-<a>))"
+					}
+				]
+			}
+		}
+	}
 };
